9aed679e72f44146662d5b7ef9731009d19efa28
Stéphane Lay <stephanelay69@gmail.com>
2025-05-10 21:55:25 +0200

    end script

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded",function(){
 
+
 let top = ["Aatrox","Ambessa","Camille","Cho'Gath","Darius","Dr.Mundo","Fiora","Gangplank","Garen","Gnar","Gragas","Gwen",
 "Illaoi","Irelia","Jax","Jayce","K'Sante","Kayle","Kennen","Kled","Malphite","Maokai","Mordekaiser","Nasus","Olaf",
 "Ornn","Pantheon","Poppy","Quinn","Renekton","Riven","Rumble","Shen","Singed","Sion","Tahm Kench","Teemo","Trundle",
@@ -14,30 +15,38 @@ let bot = ["Aphelios","Ashe","Caitlyn","Draven","Ezreal","Jhin","Jinx","Kai'sa",
 ,"Nilah","Samira","Sivir","Smolder","Tristana","Twitch","Varus","Vayne","Xayah","Zeri"];
 
 let sup = ["Alistar","Bard","Blitzcrank","Brand","Braum","Janna","Karma","Leona","Lulu","Lux","Milio","Morgana","Nami"
-,"Nautilus","Neeko","Pyke","Rakan","Rell","Renata","Sona","Soraka","Swain","Seraphine","Taric","Tresh","Vel'koz","Yuumi",
+,"Nautilus","Neeko","Pyke","Rakan","Rell","Renata","Sona","Soraka","Swain","Seraphine","Taric","Thresh","Vel'koz","Yuumi",
 "Zilean","Zyra"];
 
 let jungle = ["Amumu","Bel'Veth","Briar","Ekko","Elise","Evelynn","FiddleSticks","Graves","Hecarim","Ivern","Jarvan","Karthus",
 "Kayn","Kha'zix","Kindred","Lee Sin","Lilia","Maitre Yi","Nidalee","Nocturne","Nunu","Rammus","Rek'sai","Rengar","Shyvana",
 "Skarner","Talon","Udyr","Vi","Viego","Volibear","Warwick","Xin zhao","Zac"];
 
+let temp_arr = [];
+
 const card_container = document.getElementById("cardcontainer");
+const button_container = document.getElementById("buttoncontainer");
+const addcard_container = document.getElementById("addcardcontainer");
+
     
+
+
+
 function Randomize(_role){
     switch (_role){
-        case _top:
+        case '_top':
             DisplayRole(top);
             break;
-        case _mid:
+        case '_mid':
             DisplayRole(mid);
             break;
-        case _sup:
+        case '_sup':
             DisplayRole(sup);
             break;
-        case _bot:
+        case '_bot':
             DisplayRole(bot);
             break;
-        case _jungle:
+        case '_jungle':
             DisplayRole(jungle);
             break;
     }
@@ -45,13 +54,43 @@ function Randomize(_role){
    
 
 function DisplayRole(arr){
+    temp_arr = arr;
+    button_container.innerHTML = "";
     for (let i = 0; i < 3; i++) {
-        let champ = arr[Math.floor(Math.random()*arr.length)];
-        card_container.innerHTML = `<img src="assets/${champ}.JPG" >`;
+        PickChamp();
+}
+    addcard_container.innerHTML += '<button id="addbutton" onclick="AddPick()"> + </button>';
+}
+
+function AddPick(){
+    if (temp_arr.length>0) {
+        PickChamp();
+    }else{
+        addcard_container.innerHTML = "";
+    }
+    
+}
+
+function PickChamp(){
+    let index = Math.floor(Math.random()*temp_arr.length);
+        let champ = temp_arr[index];
+        card_container.innerHTML += `<img src="assets/${champ}.JPG">`;
+        temp_arr.splice(index,1);
+}
+
+function Reset(){
+    button_container.innerHTML = `<button onclick="Randomize('_top')">Top</button>
+        <button onclick="Randomize('_jungle')">Jungle</button>
+        <button onclick="Randomize('_mid')">Mid</button>
+        <button onclick="Randomize('_bot')">Bot</button>
+        <button onclick="Randomize('_sup')">Support</button>`;
+    
+    card_container.innerHTML = "";
+    addcard_container.innerHTML = ""
 }
 
 
-    window.Randomize = Randomize;
-
-}
+window.Reset = Reset;
+window.Randomize = Randomize;
+window.AddPick = AddPick;
 })
